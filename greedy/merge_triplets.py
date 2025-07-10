cc8811d306c02d93b8333ae61525dcd9697a760b
tima20 <tima20@vt.edu>
2025-07-10 15:21:13 -0400

    Solved "merge_triplets" problem

---
+++ greedy/merge_triplets.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        valid_triplets = []
+        has_appeared = [False, False, False]
+
+        for triplet in triplets:
+            is_valid = True
+            for i, elem in enumerate(triplet):
+                if elem > target[i]:
+                    is_valid = False
+
+            if not is_valid:
+                continue
+
+            for i, elem in enumerate(triplet):
+                if elem == target[i]:
+                    has_appeared[i] = True
+
+        return all(has_appeared)
